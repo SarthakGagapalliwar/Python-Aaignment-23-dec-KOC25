066449380124746919d7566255cf57c3ce10382e
SarthakGagapalliwar <118716600+SarthakGagapalliwar@users.noreply.github.com>
2022-12-23 11:34:49 +0530

    Add files via upload

---
+++ 15.py
@@ -0,0 +1,15 @@
+sample_dict = {
+    "name": "Kelly",
+    "age": 25,
+    "salary": 8000,
+    "city": "New york"}
+
+# Keys to extract
+keys = ["name", "salary"]
+dict1={}
+
+for i,j in sample_dict.items():
+    for k in keys:
+        if k==i:
+            dict1[i]=j
+print(dict1)
